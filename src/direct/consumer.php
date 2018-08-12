9dbe85c47f530e94b9213b02ca1f818f93d4aaa8
Onur Şimşek <posta@onursimsek.com>
2018-08-12 23:57:35 +0300

    Direct queue is done

--- src/direct/consumer.php
+++ src/direct/consumer.php
@@ -1,17 +1,17 @@
 <?php
 require_once __DIR__ . '/../connection.php';
 
+list(, $queue) = $argv;
+$queue = 'tuts-01';
 $channel = $connection->channel();
 
-$channel->queue_declare('tuts-01', false, false, false, false);
+$channel->queue_declare($queue, false, false, false, false);
 
 echo " [*] Waiting for messages. To exit press CTRL+C\n";
+$channel->basic_consume($queue, '', false, true, false, false, function ($msg) {
+    echo '[x] Received ' . $msg->body . PHP_EOL;
+});
 
-$callback = function ($msg) {
-    echo ' [x] Received ', $msg->body, "\n";
-};
-
-$channel->basic_consume('tuts-01', '', false, true, false, false, $callback);
 while (count($channel->callbacks)) {
     $channel->wait();
 }
--- src/direct/producer.php
+++ src/direct/producer.php
@@ -1,17 +1,18 @@
 <?php
 
-use PhpAmqpLib\Message\AMQPMessage;
-
 require_once __DIR__ . '/../connection.php';
 
+use PhpAmqpLib\Message\AMQPMessage;
+
+list(, $message) = $argv;
 $channel = $connection->channel();
 
 $channel->queue_declare('tuts-01', false, false, false, false);
 
-$msg = new AMQPMessage('Hello World!');
+$msg = new AMQPMessage($message);
 $channel->basic_publish($msg, '', 'tuts-01');
 
-echo " [x] Sent 'Hello World!'\n";
+echo "[x] Sent '{$message}'\n";
 
 $channel->close();
 $connection->close();
