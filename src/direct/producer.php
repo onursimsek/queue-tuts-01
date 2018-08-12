2a074027d44655b767b974422cb531efc2f3fcf3
Onur Şimşek <onur.simsek@travelbuild.com>
2018-08-12 20:53:26 +0300

    set routing-key

--- src/direct/producer.php
+++ src/direct/producer.php
@@ -9,7 +9,7 @@ $channel = $connection->channel();
 $channel->queue_declare('tuts-01', false, false, false, false);
 
 $msg = new AMQPMessage('Hello World!');
-$channel->basic_publish($msg, '', 'hello');
+$channel->basic_publish($msg, '', 'tuts-01');
 
 echo " [x] Sent 'Hello World!'\n";
 
