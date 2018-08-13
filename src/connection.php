90ec9a18f2ec58cd9f88df4259bb79231cf01b2c
Onur Şimşek <posta@onursimsek.com>
2018-08-13 03:00:08 +0300

    add channel

--- src/connection.php
+++ src/connection.php
@@ -4,3 +4,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 $connection = new AMQPStreamConnection('queue-rabbit', 5672, 'guest', 'guest');
+$channel = $connection->channel();
